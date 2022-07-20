568bbc9b1ae9bf04235d8513aea5bf9075ea3f51
Fang Xuetao <fxtaoo@gmail.com>
2022-07-20 14:15:59 +0800

    更新

--- new.sh
+++ new.sh
@@ -24,9 +24,12 @@ package main
 go mod init app/${app_name}&& \
 echo -e "$content" > ./main.go
 echo -e "$content" > ./main_test.go
-echo -e "# ${app_name} \n ${intro}" > ./README.md)
+echo -e "# ${app_name}\n${intro}\n\n## 配置\n\n## 功能\n - [ ] " > ./README.md)
 
 echo -e "| [${app_name}](${github_repository_url}/${app_name}) | ${intro} |">> ./README.md
+
+# vscode 新窗口打开
+code ./$app_name &
 
 
 
