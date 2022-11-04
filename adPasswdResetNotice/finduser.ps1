268b72c56279d795ed789ae5d9f7dee7ead0aec9
Fang Xuetao <fxtaoo@gmail.com>
2022-11-04 16:12:15 +0800

    修复天数筛选

--- adPasswdResetNotice/finduser.ps1
+++ adPasswdResetNotice/finduser.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module ActiveDirectory
 
 $maxPwdAge=(Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days
-$startMonitor=(get-date).AddDays(14-$maxPwdAge).ToShortDateString()
+$startMonitor=(get-date).AddDays(14-$maxPwdAge)
 
 
-Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0} –Properties * | where {($_.PasswordLastSet).ToShortDateString() -lt $startMonitor} | select-object SamAccountName,passwordlastset,Name | Export-Csv -Path ./userdata.csv -encoding utf8 -NoTypeInformation
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False -and PasswordLastSet -gt 0} –Properties * | where {$_.PasswordLastSet -lt $startMonitor} | select-object SamAccountName,passwordlastset,Name | Export-Csv -Path ./userdata.csv -encoding utf8 -NoTypeInformation
