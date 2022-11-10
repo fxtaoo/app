6f2d2bda11a37f437dd77e12a44e55a8767d555a
Fang Xuetao <fxtaoo@gmail.com>
2022-11-10 21:52:39 +0800

    添加命名参数，http  身份验证

--- ginRunCmd/main.go
+++ ginRunCmd/main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -21,7 +23,7 @@ type Server struct {
 
 type Servers struct {
 	ServicesTagPath string
-	ServicesTag     map[string]interface{}
+	ServicesTag     map[string]map[string]string
 	UpdateServer    Server
 }
 
@@ -31,50 +33,71 @@ func (s *Servers) read() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	err = json.Unmarshal(f, &s.ServicesTag)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-// func (s *Servers) Save() {
-// 	data, err := json.Marshal(s.ServicesTag)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 		return
-// 	}
-// 	os.WriteFile(s.ServicesTagPath, data, 0666)
-// }
-
-// func (s *Servers) update(server *Servers) {
-// 	s.ServicesTag[Server.Sort].(map[string]interface{})[server.Name] = server.tag
-// }
+func (s *Servers) save(tagPath string) {
+	data, err := json.Marshal(s.ServicesTag)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	os.WriteFile(tagPath, data, 0666)
+}
 
 func main() {
-	s := Servers{ServicesTagPath: "services_tag.json"}
-	s.read()
+	ip := flag.String("ip", "", "IP")
+	port := flag.Int("port", 9158, "端口")
+	user := flag.String("user", "", "用户名，不配置无 HTTP 基本认证")
+	passwd := flag.String("passwd", "", "密码，不配置无 HTTP 基本认证")
+	flag.Parse()
+
 	r := gin.Default()
+	if *user != "" && *passwd != "" {
+		r.Use(gin.BasicAuth(gin.Accounts{
+			*user: *passwd,
+		}))
+	}
+
 	r.LoadHTMLFiles("template/update.html")
+
 	r.GET("/test/update", func(ctx *gin.Context) {
+		s := Servers{ServicesTagPath: "services_tag.json"}
+		s.read()
+
 		ctx.HTML(200, "update.html", s)
 	})
+
 	r.POST("/test/update", func(ctx *gin.Context) {
+		s := Servers{ServicesTagPath: "services_tag.json"}
+		s.read()
+
 		server := Server{
 			Sort:   ctx.Query("sort"),
 			Name:   ctx.PostForm("name"),
 			TagNew: strings.TrimSpace(ctx.PostForm("tag")),
 		}
 
-		tmp := s.ServicesTag[server.Sort]
-		server.TagOld = tmp.(map[string]interface{})[server.Name].(string)
+		server.TagOld = s.ServicesTag[server.Sort][server.Name]
 
 		cmd := exec.Command("fab", "update", "--ij", server.Sort, "-n", server.Name, "-t", server.TagNew)
-		// cmd := exec.Command("cat","t")
-		out, _ := cmd.CombinedOutput()
+		// cmd := exec.Command("echo", "fab", "update", "--ij", server.Sort, "-n", server.Name, "-t", server.TagNew)
+		out, err := cmd.CombinedOutput()
+		if err == nil {
+			s.save(s.ServicesTagPath + ".b")
+			s.ServicesTag[server.Sort][server.Name] = server.TagNew
+			s.save(s.ServicesTagPath)
+		}
+
 		server.Result = strings.Split(string(out), "\n")
 		s.UpdateServer = server
 		ctx.HTML(200, "update.html", s)
 		s.UpdateServer = Server{}
 	})
-	r.Run(":8173")
+
+	r.Run(fmt.Sprintf("%s:%d", *ip, *port))
 }
