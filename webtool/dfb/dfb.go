304d5d8695c7565bba0e3a80337345ef6ecce6db
Fang Xuetao <fxtaoo@gmail.com>
2023-01-12 14:37:10 +0800

    url 自定义煮饭、吃饭时间

--- webtool/dfb/dfb.go
+++ webtool/dfb/dfb.go
@@ -10,6 +10,7 @@ import (
 
 type V struct {
 	DateTime string
+	Cooktime int
 	Message  string
 }
 
@@ -17,26 +18,42 @@ func Get(ctx *gin.Context) {
 	// 默认吃饭时间为明天 07:50
 	V := V{
 		time.Now().AddDate(0, 0, 1).Format("2006-01-02") + "T07:50",
-		""}
+		0,
+		"",
+	}
+
+	if ctx.Query("cooktime") != "" {
+		V.Cooktime, _ = strconv.Atoi(ctx.Query("cooktime"))
+	}
+
+	if ctx.Query("mealtime") != "" {
+		V.DateTime = fmt.Sprintf("%sT%s",
+			time.Now().AddDate(0, 0, 1).Format("2006-01-02"),
+			ctx.Query("mealtime"),
+		)
+	}
+
 	ctx.HTML(200, "dfb.html", V)
 }
 
 func Post(ctx *gin.Context) {
-	V := V{"", ""}
-	cooktime, _ := strconv.Atoi(ctx.PostForm("cooktime"))
+	V := V{
+		ctx.PostForm("mealtime"),
+		0,
+		""}
+	V.Cooktime, _ = strconv.Atoi(ctx.PostForm("cooktime"))
 
-	V.DateTime = ctx.PostForm("mealtime")
 	mealtime, _ := time.Parse("2006-01-02T15:04", V.DateTime)
 	// cst 转 utc
 	mealtime = mealtime.Add(-time.Hour * 8)
 
 	sinceMinute := int(mealtime.Sub(time.Now().UTC()).Minutes())
 
-	if sinceMinute < cooktime {
+	if sinceMinute < V.Cooktime {
 		V.Message = "煮饭时间不够！"
 	} else {
-		sinceMinute -= cooktime
-		V.Message = fmt.Sprintf("定时：%d 小时 %d 分钟", sinceMinute/60, sinceMinute%60)
+		sinceMinute -= V.Cooktime
+		V.Message = fmt.Sprintf("定时 %d 小时 %d 分钟后饭煮好", sinceMinute/60, sinceMinute%60)
 	}
 
 	ctx.HTML(200, "dfb.html", V)
